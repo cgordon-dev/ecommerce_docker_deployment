1b5b52e56fce5e573636e807c5d543aa94af1c6f
cgordon-dev <cgordon.dev@gmail.com>
2024-11-18 03:17:05 +0000

    add vpc module files

---
+++ Terraform/vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "default_vpc_id" {
+  description = "The default vpc id"
+  type        = string
+  default     = "vpc-0b40f073f41c278ad"
+}
+
+variable "default_route_table_id" {
+  description = "The default route table id"
+  type = string
+  default = "rtb-0df77378a2d08d903"
+}
